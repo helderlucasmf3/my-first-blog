e1de2240aaa6d194ee4b5a5749df15a0ed4afd17
Helder Lucas <helderlucasmf3@hotmail.com>
2018-03-24 16:09:26 -0300

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9q@5shwb2cyk3)2^_=wv-ee2b2w$%@*juzr+h!wllm!(bcody_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'helderlucasmf3.pythonanywhere.com']
 
 
 # Application definition
